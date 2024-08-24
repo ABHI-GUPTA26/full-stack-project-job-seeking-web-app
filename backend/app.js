53063ccc1ad75d5195830acf82cdb268633489e0
ABHI-GUPTA26 <158084578+ABHI-GUPTA26@users.noreply.github.com>
2024-08-24 16:08:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set("trust proxy",1);
 app.use(
   fileUpload({
     useTempFiles: true,
